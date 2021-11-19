f48b76751fca06288770e2beebc1d150ccb7faf5
neanders <nelson.andersen@microsoft.com>
2021-11-18 19:42:29 -0800

    Finished sev update script

--- random/update_sev.py
+++ random/update_sev.py
@@ -1,8 +1,24 @@
 import yaml
-import sys
-from yaml.loader import FullLoader
 
+"""
+This script was designed for the Networking-Device-Health repo
 
+This scripts takes the config-base, checks for all repair scenarios lower than 3 (sev2s, sev1s), 
+and inserts a severity override in the config-ussec file
+
+The alerting config for the different clouds is configured to use default to config-base.yml,
+but prioritize any config in the config-{cloudtype}.yml for the specific cloud. By using 
+this script, you insert "Severity": 3 for any sev 2s or sev1s in the config, overriding the
+base config
+
+NOTE: This script removes comments from the YAML file, and does weird things to certain strings
+within quotes
+"""
+
+# Key Value pair to be inserted
+upd_sev = {"Severity": 3}
+
+# Recursive loop to find Severity key in nested dictionaries
 def find(d, sev):
     if sev in d:
         yield d[sev]
@@ -11,19 +27,36 @@ def find(d, sev):
             for i in find(v, sev):
                 yield i, k
 
-config_base = open(r"C:\Networking-Device-Health\src\GreenSeerServices\GSHAutoRepair\serviceconfig\config-base.yml")
+
+# Open base config file to check for alerts configured sev 2 or lower
+config_base = open(
+    r"C:\Networking-Device-Health\src\GreenSeerServices\GSHAutoRepair\serviceconfig\config-base.yml",
+    encoding="UTF-8",
+)
 base_data = yaml.load(config_base, Loader=yaml.FullLoader)
 
-config_ussec = open(r"C:\Networking-Device-Health\src\GreenSeerServices\GSHAutoRepair\serviceconfig\config-ussec.yml")
-# config_ussec = open(r"C:\neanders-repo\random\test.yml")
+# Open ussec config file to insert severity override
+config_ussec = open(
+    r"C:\Networking-Device-Health\src\GreenSeerServices\GSHAutoRepair\serviceconfig\config-ussec.yml",
+    encoding="UTF-8",
+)
 ussec_data = yaml.load(config_ussec, Loader=yaml.FullLoader)
 
 high_sev_alert = ()
 
-for val in find(base_data, 'Severity'):
+# Logic to look for any repair scenarios in the base config, check if severity is < 3
+# and insert new severity to cloud config
+for val in find(base_data, "Severity"):
     if val[0][0][0] < 3:
         high_sev_alert = val[0][1]
         for key in ussec_data["RepairScenarios"]:
             if key in high_sev_alert:
-                print(key+" needs to be overridden")
-
+                # print(key+" needs to be overridden")
+                ussec_data["RepairScenarios"][key]["IncidentTemplate"].update(upd_sev)
+                with open("ussec_data", "w", encoding="UTF-8") as update_ussec_data:
+                    yaml.dump(
+                        ussec_data,
+                        update_ussec_data,
+                        default_flow_style=False,
+                        sort_keys=False,
+                    )
